4324967e03206d949ff6b26c0db7b634e48b2b2f
Richard Kovacs <kovacsricsi@gmail.com>
2022-09-07 10:53:31 +0200

    Fix metrics label to allow multi diskconfig (#37)

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -74,6 +74,16 @@ func RenderPVCName(elems ...string) (string, error) {
 	return builder.String(), nil
 }
 
+// RenderMetricsLabel
+func RenderMetricsLabel(name string) string {
+	hash, err := Hash(name)
+	if err != nil {
+		panic("Unable to calculate hash, better to say good bye!")
+	}
+
+	return fmt.Sprintf("discoblocks/metrics/%d", hash)
+}
+
 // IsContainsAll finds for a contains all b
 func IsContainsAll(a, b map[string]string) bool {
 	match := 0
