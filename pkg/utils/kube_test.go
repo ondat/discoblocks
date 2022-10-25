271a79cbc6cb1a3890fb548720ded84dd9d873e7
Richard Kovacs <kovacsricsi@gmail.com>
2022-10-25 09:29:44 +0200

    HostPID works, hurray

--- pkg/utils/kube_test.go
+++ pkg/utils/kube_test.go
@@ -6,12 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRenderMetricsService(t *testing.T) {
-	_, err := RenderMetricsService("name", "namespace")
-
-	assert.Nil(t, err, "invalid metrics service template")
-}
-
 func TestRenderMetricsSidecar(t *testing.T) {
 	_, err := RenderMetricsSidecar()
 
